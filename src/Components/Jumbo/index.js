8b8e64feec41f713dfe8099ed75784fb1a6752db
Sarneet Longia <sarneet.longia@gmail.com>
2019-11-03 16:44:05 -0500

    Working version with full functionality

--- src/Components/Jumbo/index.js
+++ src/Components/Jumbo/index.js
@@ -7,12 +7,12 @@ class Jumbo extends React.Component {
 render() {
   return (    
     <MDBJumbotron fluid>
-      <MDBContainer>
-        <h2 className="display-4">{this.props.message === "" ? "Will you be the true Ruler of the Seven Kingdoms?" : this.props.message }</h2> 
+      <MDBContainer className="h-75">
+        <h1 className="text-center">{this.props.message === "" ? "Will you be the true Ruler of the Seven Kingdoms?" : this.props.message }</h1> 
         <hr className="my-2" />
         <p className="lead">Rules</p>
-        <p>1) Click on a character that you have not selected</p>
-        <p>2) Continue clicking on a new (not already chosen) character until you reach 12</p>
+        <p>1) Click on a character that has not been selected</p>
+        <p>2) Try to select all character exactly once (12 in total) </p>
       </MDBContainer>
     </MDBJumbotron>
   )}
