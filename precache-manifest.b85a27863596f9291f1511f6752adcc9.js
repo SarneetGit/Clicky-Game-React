598e0711144f21eca70f43af9d9ba7690ca724bf
Sarneet Longia <sarneet.longia@gmail.com>
2019-11-03 21:26:34 -0500

    Updates

---
+++ precache-manifest.b85a27863596f9291f1511f6752adcc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566ccea7869b3c0aed008b3a701dc91c",
+    "url": "/SarneetGit/Clicky-Game-React/index.html"
+  },
+  {
+    "revision": "c59c8407c33071f007ad",
+    "url": "/SarneetGit/Clicky-Game-React/static/css/main.8ad34053.chunk.css"
+  },
+  {
+    "revision": "cec4fbb37ae58921e76b",
+    "url": "/SarneetGit/Clicky-Game-React/static/js/2.cc0a77b9.chunk.js"
+  },
+  {
+    "revision": "c59c8407c33071f007ad",
+    "url": "/SarneetGit/Clicky-Game-React/static/js/main.8d6772f5.chunk.js"
+  },
+  {
+    "revision": "07e3339dcb7014a82c9d",
+    "url": "/SarneetGit/Clicky-Game-React/static/js/runtime-main.3cec7fdd.js"
+  }
+]);
